c37943a1f3a91a17fdeceeb0cc9709e09ed9dd6d
AmetDev <ametsejdaliev@gmail.com>
2023-09-07 21:33:07 +0300

    first commit

---
+++ front-lib/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import TextInput from './TextInput'
+
+const Header = () => {
+	return (
+		<div className='flex justify-center h-[200px] text-center bg-gray-400 '>
+			<TextInput />
+		</div>
+	)
+}
+
+export default Header
---
+++ front-lib/src/components/TextInput.jsx
@@ -0,0 +1,7 @@
+const TextInput = () => {
+	return (
+		<input type='text' placeholder='Поиск...' className='h-6' />
+	)
+}
+
+export default TextInput
