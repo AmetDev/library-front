cfe80844313e909bac0902bff2538d615028ea01
AmetDev <ametsejdaliev@gmail.com>
2023-09-08 21:30:12 +0300

    header-ui

--- front-lib/src/components/Header.jsx
+++ front-lib/src/components/Header.jsx
@@ -1,34 +1,88 @@
-import '../styles/App.css';
-import Dropdown from 'react-bootstrap/Dropdown';
-import FormTextExample from './TextInput';
+import { useState } from 'react'
+import Dropdown from 'react-bootstrap/Dropdown'
+import '../styles/App.css'
+import FormTextExample from './TextInput'
+const categories = [
+	'all',
+	'art',
+	'biography',
+	'computers',
+	'history',
+	'medical',
+	'poetry',
+]
+const sortings = ['relevance', 'newest']
+const Categories = () => {
+	const [currentCategories, setCurrentCategories] = useState('')
+	const [isClick, setIsClick] = useState(false)
+	const onClickCategories = el => {
+		setCurrentCategories(el)
+		setIsClick(true)
+	}
 
-function BasicExample() {
-    return (
-        <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                Dropdown Button
-            </Dropdown.Toggle>
+	return (
+		<Dropdown>
+			<Dropdown.Toggle variant='success' id='dropdown-basic'>
+				{isClick ? currentCategories : categories[0]}
+			</Dropdown.Toggle>
 
-            <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </Dropdown.Menu>
-        </Dropdown>
-    );
+			<Dropdown.Menu>
+				{categories.map(el => {
+					return (
+						<Dropdown.Item
+							href='#/action-1'
+							onClick={() => onClickCategories(el)}
+							key={el}
+						>
+							{el}
+						</Dropdown.Item>
+					)
+				})}
+			</Dropdown.Menu>
+		</Dropdown>
+	)
+}
+const Sorting = () => {
+	const [currentSort, setCurrentSort] = useState('')
+	const [isClick, setIsClick] = useState(false)
+	const onClickSort = el => {
+		setCurrentSort(el)
+		setIsClick(true)
+	}
+	return (
+		<Dropdown>
+			<Dropdown.Toggle variant='success' id='dropdown-basic'>
+				{isClick ? currentSort : sortings[0]}
+			</Dropdown.Toggle>
+
+			<Dropdown.Menu>
+				{sortings.map(el => {
+					return (
+						<Dropdown.Item
+							href='#/action-1'
+							onClick={() => onClickSort(el)}
+							key={el}
+						>
+							{el}
+						</Dropdown.Item>
+					)
+				})}
+			</Dropdown.Menu>
+		</Dropdown>
+	)
 }
 const Header = () => {
-    return (
-        <div className="flex justify-center flex-col text-center h-[200px] text-center bg-gray-400 ">
-            <h1>Search books</h1>
-            <FormTextExample />
+	return (
+		<div className='flex justify-center items-center flex-col text-center bg-gray-400 '>
+			<h1>Search books</h1>
+			<FormTextExample />
 
-            <div className="flex space-x-[900px]">
-                <BasicExample />
-                <BasicExample />
-            </div>
-        </div>
-    );
-};
+			<div className='flex space-x-[900px]'>
+				<Categories />
+				<Sorting />
+			</div>
+		</div>
+	)
+}
 
-export default Header;
+export default Header
--- front-lib/src/components/TextInput.jsx
+++ front-lib/src/components/TextInput.jsx
@@ -1,19 +1,36 @@
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form'
+import InputGroup from 'react-bootstrap/InputGroup'
 
 function FormTextExample() {
-    return (
-        <div>
-            <InputGroup className="mb-3 w-[500px]">
-                <InputGroup.Text id="basic-addon1">Search</InputGroup.Text>
-                <Form.Control
-                    placeholder="Search"
-                    aria-label="Search"
-                    aria-describedby="basic-addon1"
-                />
-            </InputGroup>
-        </div>
-    );
+	return (
+		<div>
+			<InputGroup className='mb-3 w-[500px]'>
+				<button onClick={() => console.log('hello')}>
+					<InputGroup.Text id='basic-addon1'>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							fill='none'
+							viewBox='0 0 24 24'
+							strokeWidth='1.5'
+							stroke='currentColor'
+							class='w-6 h-6'
+						>
+							<path
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
+							/>
+						</svg>
+					</InputGroup.Text>
+				</button>
+				<Form.Control
+					placeholder='Search'
+					ariaLabel='Search'
+					ariaDescribedby='basic-addon1'
+				/>
+			</InputGroup>
+		</div>
+	)
 }
 
-export default FormTextExample;
+export default FormTextExample
